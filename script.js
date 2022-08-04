d6fc5555bf151f18acdc64bb7793770b582533bc
Lindsey Klarenbeek <104466782+klarelin@users.noreply.github.com>
2022-08-04 12:20:27 -0500

    Changes 'Star' to 'Start' on replay

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
 console.log(document.querySelector('.message').textContent);
@@ -16,65 +16,65 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
-document.querySelector('.check').addEventListener('click', function () {
+document.querySelector(".check").addEventListener("click", function () {
   // function is only called as soon as the even happens
-  const guess = Number(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
   // When no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No Number!';
+    document.querySelector(".message").textContent = "⛔ No Number!";
 
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '✨Correct Number!';
-    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector(".message").textContent = "✨Correct Number!";
+    document.querySelector(".number").textContent = secretNumber;
 
-    document.querySelector('body').style.backgroundColor = '#019267';
-    document.querySelector('.number').style.width = '30rem';
+    document.querySelector("body").style.backgroundColor = "#019267";
+    document.querySelector(".number").style.width = "30rem";
 
     if (score > highScore) {
       highScore = score;
-      document.querySelector('.highscore').textContent = highScore;
+      document.querySelector(".highscore").textContent = highScore;
     }
 
     // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        'Your number is too high!';
+      document.querySelector(".message").textContent =
+        "Your number is too high!";
       score--;
-      document.querySelector('.score').textContent = score;
+      document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector('.message').textContent =
-        'X﹏X You lost the game!';
-      document.querySelector('.score').textContent = 0;
+      document.querySelector(".message").textContent =
+        "X﹏X You lost the game!";
+      document.querySelector(".score").textContent = 0;
     }
 
     //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent =
-        'Your number is too low!';
+      document.querySelector(".message").textContent =
+        "Your number is too low!";
       score--;
-      document.querySelector('.score').textContent = score;
+      document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector('.message').textContent =
-        'X﹏X You lost the game!';
-      document.querySelector('.score').textContent = 0;
+      document.querySelector(".message").textContent =
+        "X﹏X You lost the game!";
+      document.querySelector(".score").textContent = 0;
     }
   }
 });
 
-document.querySelector('.again').addEventListener('click', function () {
+document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector('.message').textContent = 'Star guessing...';
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
 
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
